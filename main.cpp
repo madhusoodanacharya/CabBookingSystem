7e1cf5b25cd0d03a3cb25282c3c84dbe3d04b559
Shashank Chaudhary <shashankchaudhary2000@gmail.com>
2019-11-11 17:30:29 +0530

    Files names redefined and minimum distance function checked.

--- main.cpp
+++ main.cpp
@@ -4,17 +4,21 @@
 
 int main(void)
 {
-    coordinates c(25.8886, -80.1145);
-    coordinates o(25.7886, -80.2145);
-    std::cout<<c.distance(o)<<"\n";
     coordinates s(0,0,"something");
     s.random_coordinates();
     s.print_coordinates();
-    std::string add = s.get_address();
 
     coordinates cabs[COORDINATE_LIMIT];
     load_coordinates(cabs);
-    cabs[0].print_coordinates();
+    double min = 100000000;
+    std::string address;
+    for(int i = 0; i<COORDINATE_LIMIT; i++){
+        if(s.distance(cabs[i]) < min){
+            min = s.distance(cabs[i]);
+            address = cabs[i].get_address();
+        }
+    }
+    std::cout<<min<<" "<<address<<std::endl;
 
     return 0;
 }
